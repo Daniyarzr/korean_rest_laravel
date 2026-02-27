b322f3dc1e2a4105fe105f4bc33e0c3cca538fb0
Ваше Имя <156177485+daniyarka@users.noreply.github.com>
2026-02-27 10:38:56 +0400

    фикс стилей по высоте

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,9 @@
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;600;700&family=Inter:wght@300;400;500&display=swap" rel="stylesheet">    
    <link rel="stylesheet" href="{{ asset('css/profile.css') }}">
     <style>
-    
+        .content-h100{
+        min-height:100vh;
+     }
         .alert-notification {
             position: fixed;
             top: 80px;
